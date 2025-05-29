501785b9e949e944a962cd92f2edd38db8c61dfb
Yash <yashyamanappapatrot@gmail.com>
2025-05-29 22:16:28 +0530

    feat:feat/swagger update NestJS dependencies and add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Community API Backend')
+    .setDescription(
+      'A backend API for user auth, posts, comments, and login tracking, built with NestJS, TypeScript, and PostgreSQL.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
